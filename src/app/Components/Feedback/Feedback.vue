8d5405183305dd9c9de86a1febcc515c078e8962
Julia <juli.shibalova@gmail.com>
2019-07-25 19:23:55 +0200

    update feedback

--- src/app/Components/Feedback/Feedback.vue
+++ src/app/Components/Feedback/Feedback.vue
@@ -1,30 +1,43 @@
 <template>
     <div class="feedback">
-        <div class="feedback__content">
+        <div class="feedback__content flex-center">
 
             <form class="form">
-                <h3>Написать письмо директору</h3>
                 <div class="form__content">
+                    <div class="form__inner">
+                        <div class="form__title">
+                            <h3>Ответим на любые ваши вопросы!</h3>
+                            <div class="lines"></div>
+                        </div>
+                        <div class="form__image flex-center">
+                            <img src="/src/images/question.png" alt="Ответим на любые ваши вопросы!">
+                        </div>
+                    </div>
                     <div class="form__inputs">
-                        <div class="form__group">
-                            <input type="text" placeholder="Ваше имя*" required>
+                        <div>
+                            <input type="text" placeholder="Ваше имя" required>
                         </div>
-                        <div class="form__group">
-                            <input type="email" placeholder="Ваш email*" required>
+                        <div>
+                            <input type="email" placeholder="Ваш email" required>
                         </div>
-                        <div class="form__group">
+                        <div>
                             <input type="text" placeholder="Ваш номер телефона">
                         </div>
+                        <div class="textarea">
+                            <textarea placeholder="Ваше сообщение" required></textarea>
+                        </div>
+                        <div class="form-group form-group-checkbox">
+                            <label>Я согласен с <a href="#">условиями обработки персональных данных</a> 
+                                <input type="checkbox" checked>
+                                <span class="checkmark"></span>
+                            </label>
+                        </div>
+                        <div class="submit">
+                            <input type="submit" value="Отправить письмо">
+                        </div>
                     </div>
-                    <div class="form__textarea">
-                        <textarea placeholder="Ваше сообщение*" required></textarea>
-                    </div>
-                </div>
-                <div class="form__submit">
-                    <input type="submit" value="Отправить письмо">
                 </div>
             </form>
-            <img src="/src/images/feedback-man.png" alt="Написать письмо директору">
         </div>
     </div>
 </template>
