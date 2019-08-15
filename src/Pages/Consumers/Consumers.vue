e5739d89bbdd5fe3919536887ec6730044cab85e
Julia <juli.shibalova@gmail.com>
2019-08-15 17:44:09 +0200

    update structure of the consumers page

--- src/Pages/Consumers/Consumers.vue
+++ src/Pages/Consumers/Consumers.vue
@@ -14,130 +14,136 @@
     <div class="consumers__main">
       <div class="consumers__main__content">
           
-        <div class="consumers__main__info">
-          <div class="oil">
-            <img src="../../assets/images/oil.png">
-          </div>
-          <div class="plus__oil"><img src="../../assets/images/back-plus.png"></div>
-
-          <div class="title-oil">
-            <p>
-              <span class="bold">Нерафинированное масло прямого отжима</span>, лучший выбор для домашнего стола. 
-              Мы рекомендуем своих потребителей с 1990 года, представляя только качественное 
-              подсолнечное масло произведенное в солнечном Казахстане.
-            </p>
-          </div>
-
-          <div class="products__oil">
-            <div class="products__product">
-              <img src="../../assets/images/circle.svg">
-              <div class="products__text">
-                Полезные вещества. При производстве подсолнечного масла мы сохраняем все важные 
-                компоненты: фосфатиды, растительные жиры, минеральные вещества и др.
-              </div>
-
+        <div class="consumers__main__inner">
+          <div class="consumers__main__item">
+            <div class="image">
+              <div class="image__front"><img class="oil" src="../../assets/images/oil.png" height="250px" width="auto"></div>
+              <div class="image__back"><img src="../../assets/images/back-plus.png"></div>
             </div>
-            <div class="products__product">
-              <img src="../../assets/images/circle.svg">
-              <div class="products__text">
-                Первый сорт. Наше подсолнечное масло - это высококачественный товар собственного 
-                производства. Оно соответствует всем стандартам и требованиям.
-              </div>
-            </div>
-            <div class="products__product">
-              <img src="../../assets/images/circle.svg">
-              <div class="products__text">
-                Прекрасный ингредиент. Вы смело можете использовать в своих рецептах наш натуральный продукт.
-              </div>
-            </div>
-          </div>    
-        </div>
-
-        <div class="consumers__main__info">
-          <div class="semya">
-            <img src="../../assets/images/consumers_semya.png" height="700" width="auto">
-          </div>
-          <div class="plus__semya"><img src="../../assets/images/back-plus.png"></div>
-
-          <div class="title-semya">
-            <p>
-              <span class="bold">Семечки, обладающие полезными свойствами и веществами.</span>
-            </p>
-          </div>
-          <div class="products__semya">
-            <div class="products__product">
-              <img src="../../assets/images/circle.svg">
-              <div class="products__text">
-                  Мы жарим семечки воздухом. В воздух ничего невозможно добавить, таким образом 
-                  на выходе чистый и здоровый продукт.
-              </div>
-            </div>
-
-            <div class="products__product">
-              <img src="../../assets/images/circle.svg">
-              <div class="products__text">
-                Современные технологии. В процессе производства мы используем самые современные 
-                технологии, что позволяет нам быть конкурентоспособными на рынке и радовать наших 
-                потребителей. 
-              </div>
-            </div>
-
-            <div class="products__product">
-              <img src="../../assets/images/circle.svg">
-              <div class="products__text">
-                Отборное сырье. Для производства мы используем только отборное сырье кондитерских сортов, 
-                благодаря этому, семечки обладают крупной и красивой формой и имеют аппетитный запах 
-              </div>
-            </div>
-
-            <div class="products__product">
-              <img src="../../assets/images/circle.svg">
-              <div class="products__text">
-                Контроль качества. Прежде чем семечки попадают в упаковку, они проходят полный контроль 
-                на всех этапах производства. От лабораторно физико-химического анализа до финальной шлифовки.
-              </div>
-            </div>
-          </div>
             
-        </div>
+            <div class="info">
+              <div class="info__title">
+                <p>
+                  <span class="bold">Нерафинированное масло прямого отжима</span>, лучший выбор для домашнего стола. 
+                  Мы рекомендуем своих потребителей с 1990 года, представляя только качественное 
+                  подсолнечное масло произведенное в солнечном Казахстане.
+                </p>
+              </div>
 
-        <div class="consumers__main__info">
-            <div class="halva">
-              <img src="../../assets/images/halva.png" height="400" width="auto">
-            </div>
-            <div class="plus__halva"><img src="../../assets/images/back-plus.png"></div>
-            <div class="title-halva">
-              <p>
-                <span class="bold">Десерт, изготавливаемый из сахара и орехов или семян.</span> Наша халва - это лучшее лакомство для 
-                Вашего стола. Это именно тот продукт, который одновременно и полезный и вкусный.
-              </p>
-            </div>
-            <div class="products__halva">
-              <div class="products__product">
+              <div class="info__text">
                 <img src="../../assets/images/circle.svg">
-                <div class="products__text">
+                <p>
+                  Полезные вещества. При производстве подсолнечного масла мы сохраняем все важные 
+                  компоненты: фосфатиды, растительные жиры, минеральные вещества и др.
+                </p>
+
+              </div>
+
+              <div class="info__text">
+                <img src="../../assets/images/circle.svg">
+                <p>
+                  Первый сорт. Наше подсолнечное масло - это высококачественный товар собственного 
+                  производства. Оно соответствует всем стандартам и требованиям.
+                </p>
+              </div>
+
+              <div class="info__text">
+                <img src="../../assets/images/circle.svg">
+                <p>
+                  Прекрасный ингредиент. Вы смело можете использовать в своих рецептах наш натуральный продукт.
+                </p>
+              </div>
+            </div>   
+          </div>
+
+          <div class="consumers__main__item">
+            <div class="info">
+              <div class="info__title">
+                <p>
+                  <span class="bold">Семечки, обладающие полезными свойствами и веществами.</span>
+                </p>
+              </div>
+
+              <div class="info__text">
+                <img src="../../assets/images/circle.svg">
+                <p>
+                    Мы жарим семечки воздухом. В воздух ничего невозможно добавить, таким образом 
+                    на выходе чистый и здоровый продукт.
+                </p>
+              </div>
+
+              <div class="info__text">
+                <img src="../../assets/images/circle.svg">
+                <p>
+                  Современные технологии. В процессе производства мы используем самые современные 
+                  технологии, что позволяет нам быть конкурентоспособными на рынке и радовать наших 
+                  потребителей. 
+                </p>
+              </div>
+
+              <div class="info__text">
+                <img src="../../assets/images/circle.svg">
+                <p>
+                  Отборное сырье. Для производства мы используем только отборное сырье кондитерских сортов, 
+                  благодаря этому, семечки обладают крупной и красивой формой и имеют аппетитный запах 
+                </p>
+              </div>
+
+              <div class="info__text">
+                <img src="../../assets/images/circle.svg">
+                <p>
+                  Контроль качества. Прежде чем семечки попадают в упаковку, они проходят полный контроль 
+                  на всех этапах производства. От лабораторно физико-химического анализа до финальной шлифовки.
+                </p>
+              </div>
+            </div>
+              
+            <div class="image">
+              <div class="image__front"><img class="semya" src="../../assets/images/consumers_semya.png" height="250px" width="auto"></div>
+              <div class="image__back"><img src="../../assets/images/back-plus.png"></div>
+            </div>
+          </div>
+
+          <div class="consumers__main__item">
+            <div class="image">
+              <div class="image__front"><img class="halva" src="../../assets/images/halva.png" height="180" width="auto"></div>
+              <div class="image__back"><img src="../../assets/images/back-plus.png"></div>
+            </div>
+              
+            <div class="info">
+              <div class="info__title">
+                <p>
+                  <span class="bold">Десерт, изготавливаемый из сахара и орехов или семян.</span> Наша халва - это лучшее лакомство для 
+                  Вашего стола. Это именно тот продукт, который одновременно и полезный и вкусный.
+                </p>
+              </div>
+
+              <div class="info__text">
+                <img src="../../assets/images/circle.svg">
+                <p>
                   Натуральный продукт. В производстве мы не применяем никаких эмульгаторов, красителей 
                   и прочей химии и активно поддерживаем эту позицию 
-                </div>
+                </p>
 
               </div>
 
-              <div class="products__product">
+              <div class="info__text">
                 <img src="../../assets/images/circle.svg">
-                <div class="products__text">
+                <p>
                   Ценим традиции. Мы не отступаем от традиционных способов производства халвы и чтим 
                   традиции самого популярного восточного десерта.
-                </div>
+                </p>
               </div>
 
-              <div class="products__product">
+              <div class="info__text">
                 <img src="../../assets/images/circle.svg">
-                <div class="products__text">
+                <p>
                   Вкусное лакомство. Наша халва - это очень полезный и вкусный продукт, который могут 
                   употреблять в пищу как взрослые, так и дети.
-                </div>
+                </p>
               </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
