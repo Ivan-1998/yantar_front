d4ce09bd2ba7add29ade193a4466d9c0ce464686
Julia <juli.shibalova@gmail.com>
2019-08-21 13:31:01 +0200

    small fixes

--- src/Pages/Contacts/Contacts.vue
+++ src/Pages/Contacts/Contacts.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="contacts">
     <div class="contacts__inner">
-      <yandex-map 
-        :settings="settings" 
-        :coords="[50.0055,82.578]" 
-        zoom="16" 
-        style="width: 100%; height: 100%; position: absolute;" 
-        :controls="['trafficControl','typeSelector','rulerControl','fullscreenControl']" 
-        options.float="none"
-        options.position.right='auto'
-        map-type="map" 
-        @map-was-initialized="initHandler" > 
-
-        <ymap-marker 
-          marker-id="1" 
-          marker-type="placemark" 
-          :coords="[50.005497, 82.582429]" 
-          :icon="{color: 'red'}" 
-          cluster-name="1" >
-        </ymap-marker> 
-      </yandex-map>
-
       <div class="contacts__left flex-center">
         <div class="contacts__left__title">контакты</div>
 
@@ -44,6 +24,25 @@
           </p>
         </div>
       </div>
+
+      <yandex-map 
+        class="contacts__map"
+        :coords="[50.0055,82.578]" 
+        zoom="16" 
+        style="width: 100%; height: 100%;" 
+        :controls="['trafficControl','typeSelector','rulerControl','fullscreenControl']" 
+        options.float="none"
+        options.position.right='auto'
+        map-type="map"> 
+
+        <ymap-marker 
+          marker-id="1" 
+          marker-type="placemark" 
+          :coords="[50.005497, 82.582429]" 
+          :icon="{color: 'red'}" 
+          cluster-name="1" >
+        </ymap-marker> 
+      </yandex-map>
     </div>
   </div>
 </template>
