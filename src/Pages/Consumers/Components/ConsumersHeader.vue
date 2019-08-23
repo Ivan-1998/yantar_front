72d5a57f0c0f4c59e76df14a79b20ce33235bb6d
tigfamon <durnev@sibtiger.com>
2019-08-23 20:02:19 +0700

    # Consumers - Рефакторинг страницы

---
+++ src/Pages/Consumers/Components/ConsumersHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container landing-header">
+    <div>
+      <h1>{{title}}</h1>
+      <p>{{subtitle}}</p>
+    </div>
+
+    <div>
+      <img src="../../../assets/images/consumers-header.png" alt="Потребителям">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ConsumersHeader',
+  data() {
+    return {
+      title: 'Потребители знакомы с нашими первоклассными продуктами с 1990 года!',
+      subtitle: 'Мы предлагаем только качественные продукты, произведенные из отборных семян подсолнечника'
+    };
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
