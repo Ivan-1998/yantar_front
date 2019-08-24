9e93ec76b7ed9ca480e4e56931755eda8105c8a5
tigfamon <durnev@sibtiger.com>
2019-08-24 12:26:06 +0700

    # Partners - Рефакторинг страницы с Партнерами

---
+++ src/Components/Landing/LandingHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container landing-header">
+    <div>
+      <h1>{{title}}</h1>
+      <p>{{subtitle}}</p>
+    </div>
+
+    <div>
+      <img :src="img" alt="Потребителям" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LandingHeader',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      default: () => ''
+    },
+    img: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/Pages/Partners/Partners.vue
+++ src/Pages/Partners/Partners.vue
@@ -1,110 +1,65 @@
 <template>
   <div class="partners">
-    <div class="partners__header">
-      <div class="partners__header__content">
-        <div class="partners__header__text">
-          <h1>Приглашаем Вас к взаимовыгодному сотрудничеству!</h1>
-          <h2>Мы всегда открыты для любых интересных предложений и вариантов продуктивного сотрудничества!</h2>
-        </div> 
-        
-        <div class="partners__header__right"><img src="../../assets/images/partners-girl.png" alt="наши партнеры"></div>
-      </div>
-    </div>
+    <LandingHeader :title="title"
+                   :subtitle="subtitle"
+                   :img="img"
+                   class="partners-header"
+    />
 
-    <div class="partners__advantages">
-      <div class="partners__advantages__content">
-        <div class="partners__advantages__title">Почему работать с нами выгодно?</div>
-        
-        <div class="advantage">
-          <div class="advantage__img">
-            <img src="../../assets/images/success.svg">
-          </div>
-          <div class="advantage__info">
-            Уверенная позиция на рынке.
-          </div>
-        </div>
+    <div class="landing-content partners-content">
+      
+      <div class="partners-profitable__bg">
+        <div class="container partners-block partners-profitable">
+          <h2 class="partners-block__title">{{profitableTitle}}</h2>
 
-        <div class="advantage">
-          <div class="advantage__img">
-            <img src="../../assets/images/like.svg">
-          </div>
-          <div class="advantage__info">
-            Продукт высокого качества
-          </div>
-        </div>
-
-        <div class="advantage">
-          <div class="advantage__img">
-            <img src="../../assets/images/factory.svg">
-          </div>
-          <div class="advantage__info">
-            Собственное производство
-          </div>
-        </div>
-        
-        <div class="advantage">
-          <div class="advantage__img">
-            <img src="../../assets/images/employee.svg">
-          </div>
-          <div class="advantage__info">
-            Выгодные условия сотрудничества
+          <div class="partners-profitable__list">
+            <div v-for="(item, index) in profitable" :key="index" class="item">
+              <div class="item-image">
+                <img :src="item.image" alt="Выгодно!" />
+              </div>
+              <p>{{item.text}}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="partners__stages">
-      <div class="partners__stages__content">
-        <div class="partners__stages__title">Этапы производства</div>
 
-        <div class="stage">
-          <div class="stage__img flex-center">
-            <img src="../../assets/images/flowers.svg">
-          </div>
-          <div class="stage__info">
-            Выращивание урожая на поле
-          </div>
-        </div>
-        
-        <div class="stage flex-center"><img src="../../assets/images/line1.svg"></div>
+      <div class="container partners-block partners-terms">
+        <h2 class="partners-block__title">{{termsTitle}}</h2>
 
-        <div class="stage">
-          <div class="stage__img flex-center">
-            <img src="../../assets/images/harvester.svg">
-          </div>
-          <div class="stage__info">
-            Сбор и хранение сырья
-          </div>
-        </div>
+        <div class="partners-terms__list">
+          <div v-for="(item, index) in terms" :key="index">
 
-        <div class="stage flex-center"><img src="../../assets/images/line2.svg"></div>
+            <div class="item">
+              <div class="item-image">
+                <img :src="item.image" :alt="`Этап производства№${index + 1}`" />
+              </div>
+              <p>{{item.text}}</p>
+            </div>
 
-        <div class="stage">
-          <div class="stage__img flex-center">
-            <img src="../../assets/images/conveyor.svg">
-          </div>
-          <div class="stage__info">
-            Производство продуктов сырья
-          </div>
-        </div>
+            <div v-if="index + 1 !== terms.length" class="line">
+              <img src="../../assets/images/partners-terms-line.svg" alt="Следующий этап" />
+            </div>
 
-        <div class="stage flex-center"><img src="../../assets/images/line3.svg"></div>
-
-        <div class="stage">
-          <div class="stage__img flex-center">
-            <img src="../../assets/images/store.svg">
-          </div>
-          <div class="stage__info">
-            Дистрибьюция готового продукта
           </div>
         </div>
       </div>
+
+
     </div>
   </div>
 </template>
 
 <script>
+import MixinParntersData from '../../Mixins/MixinPartnersData';
+import LandingHeader from '../../Components/Landing/LandingHeader';
+
+
 export default ({
-  name: 'Partners'
+  name: 'Partners',
+  mixins: [MixinParntersData],
+  components: {
+    LandingHeader
+  }
 });
 </script>
