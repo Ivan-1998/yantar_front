33773e4418544e5eff741ccb7aed7e6bae155a10
tigfamon <durnev@sibtiger.com>
2019-09-05 15:05:42 +0700

    # Feedback.vue - рефакторинг формы обратной связи

--- src/Components/Feedback/Feedback.vue
+++ src/Components/Feedback/Feedback.vue
@@ -1,45 +1,35 @@
 <template>
   <div class="feedback">
-    <div class="feedback__content flex-center">
-
-      <form class="form">
-        <div class="form__content">
-          <div class="form__inner">
-            <div class="form__title">
-              <h3>Ответим на любые ваши вопросы!</h3>
-              <div class="lines"></div>
-            </div>
-            <div class="form__image flex-center">
-              <img src="../../assets/images/question.svg" alt="Ответим на любые ваши вопросы!">
-            </div>
-          </div>
-
-          <div class="form__inputs">
-            <div class="form-group">
-              <input type="text" placeholder="Ваше имя" required>
-            </div>
-            <div class="form-group">
-              <input type="email" placeholder="Ваш email" required>
-            </div>
-            <div class="form-group">
-              <input type="text" placeholder="Ваш номер телефона">
-            </div>
-            <div class="form-group form-group__textarea">
-              <textarea placeholder="Ваше сообщение" required></textarea>
-            </div>
-            <div class="form-group form-group__checkbox">
-              <label>Я согласен с <a href="#">условиями обработки персональных данных</a> 
-                <input type="checkbox" checked>
-                <span class="checkmark"></span>
-              </label>
-            </div>
-              <div class="form-group">
-                <input type="submit" value="Отправить письмо">
-              </div>
-          </div>
+    <form class="feedback-form">
+      <div class="feedback-left">
+        <h2>Ответим на любые ваши вопросы!</h2>
+        <img src="../../assets/images/feedback.svg" alt="Оставьте заявку!" />
+      </div>
+      <div class="feedback-right">
+        <div class="form-group">
+          <input type="email" placeholder="Электронный адрес" />
         </div>
-      </form>
-    </div>
+        <div class="form-group">
+          <input type="text" placeholder="Имя" />
+        </div>
+        <div class="form-group">
+          <input type="text" placeholder="Номер телефона" />
+        </div>
+        <div class="form-group">
+          <textarea placeholder="Сообщение"></textarea>
+        </div>
+        <div class="form-group form-group__checkbox">
+          <label>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+          <p>Я согласен с условиями <a href="#">политики конфиденциальности</a></p> 
+        </div>
+        <div class="form-group">
+          <button type="submit">Отправить письмо</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
